7660183c0f6b3c78e9c7399ba2f6a71f34339ca0
Jbrow95 <browningjustin95@gmail.com>
2018-09-13 12:39:08 -0400

    tested if cards has 52, and if suits have 4

--- poker.rb
+++ poker.rb
@@ -2,12 +2,14 @@ class Deck
     def initialize
         @cards = []
         values = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
-        suits = ['C', 'D', 'H', 'S']
+        @suits = ['C', 'D', 'H', 'S']
         suits.each do |s|
             values.each do |v|
                 @cards << v + s
             end
         end
     end
+
     attr_reader :cards
+    attr_reader :suits
 end
--- poker_tdd.rb
+++ poker_tdd.rb
@@ -10,4 +10,14 @@ class TestIsbn < Minitest::Test
         deck = Deck.new
         assert_equal(Array, deck.cards.class)
     end
+
+    def test_if_deck_has_52cards
+        deck = Deck.new
+        assert_equal(52, deck.cards.length)
+    end
+
+    def test_if_deck_has_4_suits
+        deck = Deck.new
+        assert_equal(4, deck.suits.length)
+    end
 end
