1ff3854bd771e03e93247def1c3a66f79c28e8ec
Jbrown95 <browningjustin95@gmail.com>
2018-12-08 23:49:08 -0500

    Refactored deal function

--- poker.rb
+++ poker.rb
@@ -2,24 +2,11 @@ class Deck
   attr_reader :cards
 
   def initialize
-    values = {
-      "2" => 2,
-      "3" => 3,
-      '4' => 4,
-      '5' => 5,
-      '6' => 6,
-      '7' => 7,
-      '8' => 8,
-      '9' => 9,
-      'T' => 10,
-      'J' => 11,
-      'Q' => 12,
-      'K' => 13,
-      'A' => 14
-    }
+    values = {"2" => 2, "3" => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, 'T' => 10, 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14 }
 
     suits = ['C', 'H', 'D', 'S']
     values2 = {}
+    
     values.each do |k, v|
       suits.each do |s|
         new_key = k + s
@@ -30,21 +17,18 @@ class Deck
     @cards = values2
     deal()
   end
+
   def deal()
     @black = []
     @white = []
     counter = 0
-    @cards.keys.shuffle.each do |k|
-      if counter.even? == true
-        @black << k
-      else
-        @white << k
-      end
 
-      if counter == 9
-        break
-      end
-      counter += 1
+    @cards.keys.shuffle.each do |k|
+    
+    counter.even? ? @black << k : @white << k
+  
+    counter == 9 ? break : counter += 1
+
     end
     @black
     @white
